8d2f019d92c3fefe0fa656d83fa2ac620bb2564f
divyavala <divyavala@users.noreply.github.com>
2022-03-04 05:16:38 +0530

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -16,44 +16,7 @@ import { FlatList } from "react-native-gesture-handler";
 let posts = require("./temp_posts.json");
 
 export default class Feed extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    componentDidMount() { }
-
-    renderItem = ({ item: post }) => {
-        return <PostCard post={post} />;
-    };
-
-    keyExtractor = (item, index) => index.toString();
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <SafeAreaView style={styles.droidSafeArea} />
-                <View style={styles.appTitle}>
-                    <View style={styles.appIcon}>
-                        <Image
-                            source={require("../assets/logo.png")}
-                            style={styles.iconImage}
-                        ></Image>
-                    </View>
-                    <View style={styles.appTitleTextContainer}>
-                        <Text style={styles.appTitleText}>Spectagram</Text>
-                    </View>
-                </View>
-                <View style={styles.cardContainer}>
-                    <FlatList
-                        keyExtractor={this.keyExtractor}
-                        data={posts}
-                        renderItem={this.renderItem}
-                    />
-                </View>
-            </View>
-        );
-    }
+   
 }
 
 const styles = StyleSheet.create({
